6a1e344c62cb68ebfbac9a4b8406353fb3fddf4f
junho.m <junho.m@navercorp.com>
2023-03-18 19:50:25 +0900

    model pl

--- model.py
+++ model.py
@@ -10,11 +10,11 @@ class atmNet(pl.LightningModule):
         self.args = args
         self.experiment = args.experiment
         self.model = None
-
         if 'pf' in self.experiment:
             pass
         elif 'timm' in self.experiment:
             self.model = self.set_model_timm()
+        self.metric_single, _ = get_metrics(n_classes=self.args.n_classes)
 
     def set_model_timm(self):
         import models.from_timm as timm
@@ -69,3 +69,39 @@ class atmNet(pl.LightningModule):
 
         return total_loss
     
+    def validation_step(self, batch, batch_idx):
+        self.shared_eval_step(batch=batch)
+
+    def test_step(self, batch, batch_idx):
+        self.shared_eval_step(batch=batch)
+
+    def on_validation_epoch_end(self):
+        self.shared_eval_epoch_end(mode='val')
+    
+    def on_test_epoch_end(self) -> None:
+        self.shared_eval_epoch_end(mode='test')
+
+    def shared_eval_step(self, batch):
+        idx, x_val, y_val = batch
+        logits = self(x_val)
+        self.metric_single.update(logits, y_val)
+    
+    def shared_eval_epoch_end(self, mode):
+        metric_out = self.metric_single.compute()
+        self.torchmetric_logger(result_dict=metric_out, mode=mode)
+        self.metric_single.reset()
+
+    def torchmetric_logger(self, result_dict, mode, multilabel=False):
+        if not multilabel:
+            self.log('{}_acc_top1'.format(mode), result_dict['acc_top1'].cpu().item(), sync_dist=True)
+            self.log('{}_acc_top5'.format(mode), result_dict['acc_top5'].cpu().item(), sync_dist=True)
+            self.log('{}_precision'.format(mode), result_dict['precision'].cpu().item(), sync_dist=True)
+            self.log('{}_recall'.format(mode), result_dict['recall'].cpu().item(), sync_dist=True)
+            self.log('{}_f1_macro'.format(mode), result_dict['f1'].cpu().item(), sync_dist=True)
+        else:
+            self.log('{}_Multi Label Precision4'.format(mode), result_dict['multi_precision_4'].cpu().item(), sync_dist=True)
+            self.log('{}_Multi Label Precision5'.format(mode), result_dict['multi_precision_5'].cpu().item(), sync_dist=True)
+            self.log('{}_Multi Label Precision6'.format(mode), result_dict['multi_precision_6'].cpu().item(), sync_dist=True)
+            self.log('{}_Multi Label Recall4'.format(mode), result_dict['multi_recall_4'].cpu().item(), sync_dist=True)
+            self.log('{}_Multi Label Recall5'.format(mode), result_dict['multi_recall_5'].cpu().item(), sync_dist=True)
+            self.log('{}_Multi Label Recall6'.format(mode), result_dict['multi_recall_6'].cpu().item(), sync_dist=True)
